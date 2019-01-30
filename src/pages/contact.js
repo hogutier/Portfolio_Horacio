64d3da15c819b1cf8b1239b3e052717ea6c810c2
Horacio Gutierrez <hogutier@yahoo.com>
2019-01-30 13:48:09 -0600

    complete about page w/o styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,66 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Link } from 'gatsby'
 
 export default () => (
   <Layout>
     <div style={{ color: 'teal' }}>
-      <h1>Contact Me</h1>
+      <h2>Let's Work Together</h2>
+      <p>
+        Send me a message if you would like to discuss a project or want to find
+        out more about me or my work, get in touch!
+      </p>
     </div>
-    <Link to="/">Home</Link>
+
+    <form method="post" action="#">
+      <div className="field half first">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" />
+      </div>
+      <div className="field half first">
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" />
+      </div>
+      <div className="field half">
+        <label htmlFor="phone">Phone</label>
+        <input type="text" name="phone" id="phone" />
+      </div>
+      <div className="field half">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+      <div className="field">
+        <label htmlFor="description">Project Description</label>
+        <p>
+          <textarea name="description" id="description" rows="6" />
+        </p>
+      </div>
+      <fieldset>
+        <legend>Type of Service</legend>
+        <p>
+          <label htmlFor="service">Select list</label>
+          <select id="myList">
+            <option value="1">Consulting</option>
+            <option value="2">UX/UI Design</option>
+            <option value="3">Software Development</option>
+            <option value="4">Product Management</option>
+          </select>
+        </p>
+      </fieldset>
+      <fieldset>
+        <legend>Project Start Date</legend>
+        <p>
+          <label htmlFor="startDate">Select list</label>
+          <select id="startDate">
+            <option value="1">Less than 3 months</option>
+            <option value="2">Between 3 to 6 months</option>
+            <option value="3">Between 6 months to 1 yr</option>
+            <option value="4">More than 1yr</option>
+          </select>
+        </p>
+      </fieldset>
+      <div>
+        <input type="submit" value="Submit" className="special" />
+      </div>
+    </form>
   </Layout>
 )
