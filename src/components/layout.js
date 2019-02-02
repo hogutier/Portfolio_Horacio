183200e8538351d3cb467993b3da8d7f5ba519a0
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-02 11:21:23 -0600

    index styling part 1

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql, Link } from 'gatsby'
 
+import logoMessage from '../images/logoMessage.png'
+import logoTwitter from '../images/logoTwitter.png'
+import logoLinkedin from '../images/logoLinkedin.png'
+import logoGithub from '../images/logoGithub.png'
 import Header from './header'
 import './layout.css'
 
@@ -32,22 +36,33 @@ const Layout = ({ children }) => (
             margin: '0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
+            paddingTop: 0,
+            color: '#B6B6B6',
+            backgroundColor: '#191919'
           }}
         >
           <div>{children}</div>
 
           <footer>
             <h2>Connect with me</h2>
-            <Link to="/contact/">Message</Link>
+            <Link to="/contact/">
+              <img src={logoMessage} alt="Message" />
+            </Link>
             <div />
-            <a href="https://www.linkedin.com/in/hogutier/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/hogutier/">
+              <img src={logoLinkedin} alt="Linkedin" />
+            </a>
             <div />
-            <a href="https://twitter.com/Horacio_Gtz">Twitter</a>
+            <a href="https://twitter.com/Horacio_Gtz">
+              <img src={logoTwitter} alt="Twitter" />
+            </a>
             <div />
             <div />
-            <a href="https://github.com/hogutier">Github</a>
-            <div />© {new Date().getFullYear()} Horacio Gutierrez{' '}
+            <a href="https://github.com/hogutier">
+              <img src={logoGithub} alt="Github" />
+            </a>
+            <div />
+            <p>© {new Date().getFullYear()} Horacio Gutierrez</p>
           </footer>
         </div>
       </>
