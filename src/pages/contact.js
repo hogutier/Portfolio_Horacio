981006102886d186f4ddd6a1abd9412959c5dc6f
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-06 17:10:32 -0600

    contact form functionality

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,79 +1,165 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { navigateTo } from 'gatsby-link'
 import '../styles/contact.css'
 
-export default () => (
-  <Layout>
-    <section className="contactContainer">
-      <div>
-        <h2>Let's Work Together</h2>
-        <p>
-          Send me a message if you would like to discuss a project or want to
-          find out more about me or my work, get in touch!
-        </p>
-      </div>
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-      <form
-        name="HoracioContactForm"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <p style={{ display: 'none' }}>
-          <label>
-            Don't fill this out: <input name="bot-field" />
-          </label>
-        </p>
-        <div className="first_name">
-          <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" id="firstName" />
-        </div>
-        <div className="last_name">
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" id="lastName" />
-        </div>
-        <div className="phone_number">
-          <label htmlFor="phone">Phone</label>
-          <input type="text" name="phone" id="phone" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-        </div>
-        <div className="field">
-          <label htmlFor="description">Project Description</label>
-          <p>
-            <textarea name="description" id="description" rows="6" />
-          </p>
-        </div>
-        <fieldset>
-          <legend>Type of Service</legend>
-          <p>
-            <label htmlFor="service">Select list</label>
-            <select id="myList">
-              <option value="1">Consulting</option>
-              <option value="2">UX/UI Design</option>
-              <option value="3">Software Development</option>
-              <option value="4">Product Management</option>
-            </select>
-          </p>
-        </fieldset>
-        <fieldset>
-          <legend>Project Start Date</legend>
-          <p>
-            <label htmlFor="startDate">Select list</label>
-            <select id="startDate">
-              <option value="1">Less than 3 months</option>
-              <option value="2">Between 3 to 6 months</option>
-              <option value="3">Between 6 months to 1 yr</option>
-              <option value="4">More than 1yr</option>
-            </select>
-          </p>
-        </fieldset>
-        <div>
-          <input type="submit" value="Submit" className="special" />
-        </div>
-      </form>
-    </section>
-  </Layout>
-)
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value })
+  }
+
+  handleSubmit = evt => {
+    evt.preventDefault()
+    const form = evt.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section className="contactContainer">
+          <div>
+            <h2>Let's Work Together</h2>
+            <p>
+              Send me a message if you would like to discuss a project or want
+              to find out more about me or my work, get in touch!
+            </p>
+          </div>
+
+          <form
+            name="HoracioContactForm"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+
+            <input type="hidden" name="form-name" value="HoracioContactForm" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{' '}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
+            <div className="first_name">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="last_name">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="phone_number">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="description">
+              <label htmlFor="description">Project Description</label>
+              <p>
+                <textarea
+                  name="description"
+                  id="description"
+                  rows="6"
+                  onChange={this.handleChange}
+                />
+              </p>
+            </div>
+            <fieldset>
+              <legend>Type of Service</legend>
+              <p>
+                <label htmlFor="service">Select list</label>
+                <select name="service" id="myList" onChange={this.handleChange}>
+                  <option name="service" value="1">
+                    Consulting
+                  </option>
+                  <option name="service" value="2">
+                    UX/UI Design
+                  </option>
+                  <option name="service" value="3">
+                    Software Development
+                  </option>
+                  <option name="service" value="4">
+                    Product Management
+                  </option>
+                </select>
+              </p>
+            </fieldset>
+            <fieldset>
+              <legend>Project Start Date</legend>
+              <p>
+                <label htmlFor="startDate">Select list</label>
+                <select
+                  name="startDate"
+                  id="startDate"
+                  onChange={this.handleChange}
+                >
+                  <option name="startDate" value="1">
+                    Less than 3 months
+                  </option>
+                  <option name="startDate" value="2">
+                    Between 3 to 6 months
+                  </option>
+                  <option name="startDate" value="3">
+                    Between 6 months to 1 yr
+                  </option>
+                  <option name="startDate" value="4">
+                    More than 1yr
+                  </option>
+                </select>
+              </p>
+            </fieldset>
+            <div>
+              <button className="button">Submit</button>
+            </div>
+          </form>
+        </section>
+      </Layout>
+    )
+  }
+}
---
+++ src/pages/thanks.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>Your information was successfully submitted!</h1>
+    <p>Thanks for reaching out. I will get in touch soon.</p>
+  </div>
+)
