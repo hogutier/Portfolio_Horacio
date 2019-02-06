b80f4c0eee97fcc4b4d908f8d987974f41be9467
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-06 17:29:32 -0600

    thank you page update

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 export default () => (
-  <div>
-    <h1>Your information was successfully submitted!</h1>
-    <p>Thanks for reaching out. I will get in touch soon.</p>
-  </div>
+  <Layout>
+    <div>
+      <h3>Your information was successfully submitted!</h3>
+      <p>Thanks for reaching out. I will get in touch with you soon.</p>
+    </div>
+  </Layout>
 )
