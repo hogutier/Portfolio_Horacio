96626b44eef89316a2ea264e50dbd21ddc002640
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-06 13:53:17 -0600

    complete portfolio grid

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,24 +3,44 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import '../styles/portfolio.css'
+import iconRealEstate from '../images/iconRealEstate.png'
+import iconBusyBus from '../images/iconBusyBus.png'
+import iconPortfolio from '../images/iconPortfolio.png'
 
 const PortfolioPage = () => (
   <Layout>
     <section className="portfolioContainer">
-      <div>
+      <div className="propertyVault">
         <Link to="/propertyvault/">
+          <img src={iconRealEstate} alt="Property Vault Project" />
           <h3>Property Vault</h3>
+          <p>
+            UX/UI Design & <br />
+            Branding
+          </p>
         </Link>
-        <p>UX/UI Design & Branding</p>
       </div>
 
-      <div>
-        <h3>Busy Bus</h3>
-        <p>UX/UI Design & Development</p>
+      <div className="busyBus">
+        <Link to="/propertyvault/">
+          <img src={iconBusyBus} alt="Busy Bus Project" />
+          <h3>Public Transportation</h3>
+          <p>
+            UX/UI Design & <br />
+            Development
+          </p>
+        </Link>
       </div>
-      <div>
-        <h3>Personal Portfolio</h3>
-        <p>UX/UI Design, Branding & Branding</p>
+
+      <div className="personalPortfolio">
+        <Link to="/propertyvault/">
+          <img src={iconPortfolio} alt="Portfolio Project" />
+          <h3>Portfolio</h3>
+          <p>
+            UX/UI Design, Branding & <br />
+            Development
+          </p>
+        </Link>
       </div>
     </section>
   </Layout>
