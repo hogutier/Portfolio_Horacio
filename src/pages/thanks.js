135ec8ef3ce72181e7d1935b11d745b506cb7a85
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-06 18:04:06 -0600

    complete thank you styling

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import '../styles/thanks.css'
 
 export default () => (
   <Layout>
-    <div>
-      <h3>Your information was successfully submitted!</h3>
-      <p>Thanks for reaching out. I will get in touch with you soon.</p>
+    <div className="thankyoucontainer">
+      <div className="thankyounote">
+        <h3>Your information was successfully submitted!</h3>
+        <p>Thanks for reaching out. I will get in touch with you soon.</p>
+      </div>
     </div>
   </Layout>
 )
