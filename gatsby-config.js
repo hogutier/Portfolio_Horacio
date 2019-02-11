76fd516093697e0fd5a0d9a60cbac0df9d7f572c
Horacio Gutierrez <hogutier@yahoo.com>
2019-02-10 18:26:13 -0600

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
-        anonymize: true,
+        anonymize: false,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
